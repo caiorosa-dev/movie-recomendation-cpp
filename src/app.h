8941d2571f46d184b0d1ccf85bea4f438e92c187
Bugado <addminplayerbr1122@gmail.com>
2024-04-10 15:29:19 -0300

    feat: Código corrigido

--- src/app.h
+++ src/app.h
@@ -4,9 +4,15 @@
 
 #include "utils/StaticList.h"
 #include "importer/MovieImporter.h"
+#include "utils/ConsoleColors.h"
 
 void execute() {
+    ConsoleColor::set(Color::LIGHT_MAGENTA);
+    cout << "[APP] Iniciando serviço..." << endl;
+
+    ConsoleColor::set(Color::LIGHT_BLUE);
     MovieImporter::import("../dataset/movies.csv");
 
+    ConsoleColor::set(Color::LIGHTGRAY);
     Movies::movies.print();
 }
--- src/populators/UserPopulator.cpp
+++ src/populators/UserPopulator.cpp
@@ -1,32 +1,34 @@
 #include "UsersPopulator.h"
 
-StaticList<String, 10> listNames;
-StaticList<String, 10> listLastNames;
+using namespace std;
+
+StaticList<string, 10> listNames;
+StaticList<string, 10> listLastNames;
 
 void populateNames() {
-    listNames.add("Sofia");
-    listNames.add("Mateus");
-    listNames.add("Isabela");
-    listNames.add("Gabriel");
-    listNames.add("Laura");
-    listNames.add("Lucas");
-    listNames.add("Giovanna");
-    listNames.add("Pedro");
-    listNames.add("Maria");
-    listNames.add("João");
+    listNames.insertAtEnd("Sofia");
+    listNames.insertAtEnd("Mateus");
+    listNames.insertAtEnd("Isabela");
+    listNames.insertAtEnd("Gabriel");
+    listNames.insertAtEnd("Laura");
+    listNames.insertAtEnd("Lucas");
+    listNames.insertAtEnd("Giovanna");
+    listNames.insertAtEnd("Pedro");
+    listNames.insertAtEnd("Maria");
+    listNames.insertAtEnd("João");
 }
 
 void populateLastNames() {
-    listLastNames.add("Silva");
-    listLastNames.add("Santos");
-    listLastNames.add("Oliveira");
-    listLastNames.add("Pereira");
-    listLastNames.add("Costa");
-    listLastNames.add("Martins");
-    listLastNames.add("Ferreira");
-    listLastNames.add("Rodrigues");
-    listLastNames.add("Almeida");
-    listLastNames.add("Carvalho");
+    listLastNames.insertAtEnd("Silva");
+    listLastNames.insertAtEnd("Santos");
+    listLastNames.insertAtEnd("Oliveira");
+    listLastNames.insertAtEnd("Pereira");
+    listLastNames.insertAtEnd("Costa");
+    listLastNames.insertAtEnd("Martins");
+    listLastNames.insertAtEnd("Ferreira");
+    listLastNames.insertAtEnd("Rodrigues");
+    listLastNames.insertAtEnd("Almeida");
+    listLastNames.insertAtEnd("Carvalho");
 }
 
 void populateUsers() {
@@ -34,12 +36,13 @@ void populateUsers() {
     populateLastNames();
 
     for (int i = 0; i < 100; ++i) {
-        UsersPopulator user;
+        Users::User user;
+
         user.name = listNames.getRandom() + " " + listLastNames.getRandom();
-        user.age = rand() % 70;
+        user.age = rand() % 50 + 18;
         user.rating = rand() % 5;
         // user.genres = genreNamesInPortuguese.getRandom();
 
-        movies.add(user);
+        movies.insertAtEnd(user);
     }
 }
