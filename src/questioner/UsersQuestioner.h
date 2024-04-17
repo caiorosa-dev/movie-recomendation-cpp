6f4b94ff4f000589bd72975b516181c115e1a909
Joshuah Leite <joshuahleite05@gmail.com>
2024-04-17 00:07:57 -0300

    UserQuestioner. still working on it

--- src/questioner/UsersQuestioner.h
+++ src/questioner/UsersQuestioner.h
@@ -31,21 +31,45 @@ namespace UsersQuestioner {
             cin >> user.age;
             cout << "Gêneros Disponiveis:";
             for(int j=0; j < MovieGenre.getSize();j++) {
-
+                // talvez n de de usar a funcao ja criada, pq acho q seria necessario uma numeracao juntamente com o genero
+                // para as escolhas
             };
             cout << "Selecione 3 gêneros desejados: ";
             cin >> response;
             StringUtils::splitString(response, ',');
-            cout << "===| Cátalogo de filmes: |===";
+            cout << "===| Filmes para assistir mais tarde: |===";
+            cout << "Selecione:";
 
             int k = 1;
             string navigation;
             while(k) {
                 Movies::movies.paginate(10,k);
-                cout << "Digite:\n Anterior\tPróximo";
+                cout << "Digite:\n Anterior\tPróximo\n (Digite Avançar para prosseguir)";
                 cin >> navigation;
                 if(navigation == "Próximo") k++;
                 else if(navigation == "Anterior") k--;
+                else if(navigation == "prosseguir") break;
+                else {
+                    cout << "Digite o número correspondente ao filme desejado: ";
+                    cin >> k;
+                    // fazer uma funcao que pega o filme pela posicao, onde k é o valor da posicao
+                    // assim, ele vai selecionar um filme no catalogo por numero e vai ser adicionado em user o nome do filme
+
+                    // insercao: digite um filme e qnd digitado armazena em uma lista e volta para a pagina 1
+                }
+            }
+            cout << "===| Filmes Favoritos: |===";
+            cout << "Selecione:";
+
+            int k = 1;
+            string navigation;
+            while(k) {
+                Movies::movies.paginate(10,k);
+                cout << "Digite:\n Anterior\tPróximo\n (Digite Avançar para prosseguir)";
+                cin >> navigation;
+                if(navigation == "Próximo") k++;
+                else if(navigation == "Anterior") k--;
+                else if(navigation == "prosseguir") break;
                 else {
                     cout << "Digite o número correspondente ao filme desejado: ";
                     cin >> k;
